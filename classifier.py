f98982da4c7a60785401a109e3388989fa7b5063
Agung <nurdiansyahagung277@gmail.com>
2026-06-23 23:23:58 +0700

    refining classifier

--- classifier.py
+++ classifier.py
@@ -4,6 +4,12 @@ import string
 import torch
 from transformers import AutoModelForSequenceClassification, AutoTokenizer, pipeline
 
+model_path = "../models"
+
+model = AutoModelForSequenceClassification.from_pretrained(model_path)
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 
 def preprocessing(text):
     for punct in string.punctuation:
@@ -11,21 +17,11 @@ def preprocessing(text):
             text = text.replace(punct, f" {punct} ")
 
     text = re.sub(" +", " ", text)
-    text = re.sub("\n", " ", text)
+    text = re.sub("[\xa0\n]", " ", text)
     return text.strip()
 
 
-def key_word():
-    pass
-
-
 def predict(data):
-    model_path = "../models"
-
-    model = AutoModelForSequenceClassification.from_pretrained(model_path)
-    tokenizer = AutoTokenizer.from_pretrained(model_path)
-
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     classifier = pipeline(
         "text-classification", model=model, tokenizer=tokenizer, device=device
     )
